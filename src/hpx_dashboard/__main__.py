df33f9d72711eb557647575da2e8f11d27c8be4e
Joachim K <jokteur@gmail.com>
2020-07-13 15:00:18 +0200

    Add copyright info

--- src/hpx_dashboard/__main__.py
+++ src/hpx_dashboard/__main__.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# HPX - dashboard
+#
+# Copyright (c) 2020 - ETH Zurich
+# All rights reserved
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 import sys
 
 from .common.logger import Logger
